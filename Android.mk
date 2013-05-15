2a6ec394ce0f16af30f7ddecae562bb7a2dde117
U-yuifan-PC\yuifan <yuifan@yuifan-PC.(none)>
2013-05-15 00:03:04 -0700

    update 4.2

--- Android.mk
+++ Android.mk
@@ -1,11 +1,6 @@
-ifneq ($(TARGET_SIMULATOR),true)
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= ping.c
+LOCAL_SRC_FILES:= ping.c ping_common.c
 LOCAL_MODULE := ping
-LOCAL_STATIC_LIBRARIES := libcutils libc
 include $(BUILD_EXECUTABLE)
-
-endif  # TARGET_SIMULATOR != true
